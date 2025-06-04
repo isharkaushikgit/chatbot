a1f9333af8c55669c2985d9965ea2683c9841c9f
isharkaushikgit <ishar.kaushik@gmail.com>
2025-06-05 00:51:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import os
 st.write("dkdkdkdy")
 
 
-os.environ["OPENAI_API_KEY"]='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'
+os.environ["OPENAI_API_KEY"]='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'
 from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(
