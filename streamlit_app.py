abd556aa966844c924355c0ee721b1a5bf5ad423
isharkaushikgit <ishar.kaushik@gmail.com>
2025-06-05 01:31:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,33 +34,34 @@ openai_api_key = st.secrets.get("open_api_key")
 
 topic = st.text_input("topic pleasee")
 
-import getpass
-import os
-
-os.environ["OPENAI_API_KEY"]=openai_api_key
-from langchain_openai import ChatOpenAI
-
-llm = ChatOpenAI(
-    model="gpt-4o",
-    temperature=0,
-    max_tokens=None,
-    timeout=None,
-    max_retries=2,
-    # api_key="...",  # if you prefer to pass api key in directly instaed of using env vars
-    # base_url="...",
-    # organization="...",
-    # other params...
-)
-
-messages = [
-    (
-        "system",
-        "You are a poet. Write 4 lines poem using the supplied topic",
-    ),
-    ("human", topic),
-]
-ai_msg = llm.invoke(messages)
-st.write(ai_msg.content)
+if topic:
+    import getpass
+    import os
+    
+    os.environ["OPENAI_API_KEY"]=openai_api_key
+    from langchain_openai import ChatOpenAI
+    
+    llm = ChatOpenAI(
+        model="gpt-4o",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+        # api_key="...",  # if you prefer to pass api key in directly instaed of using env vars
+        # base_url="...",
+        # organization="...",
+        # other params...
+    )
+    
+    messages = [
+        (
+            "system",
+            "You are a poet. Write 4 lines poem using the supplied topic",
+        ),
+        ("human", topic),
+    ]
+    ai_msg = llm.invoke(messages)
+    st.write(ai_msg.content)
 
 
 
